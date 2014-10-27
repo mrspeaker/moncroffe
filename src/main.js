702adc7d551317876ff4e2c1a15f459930b7fc96
Mr Speaker <mrspeaker@gmail.com>
2014-10-27 19:33:25 -0400

    light follows camera

--- src/main.js
+++ src/main.js
@@ -362,9 +362,11 @@ var main = {
 		var ambientLight = new THREE.AmbientLight(this.day ? 0x888888 : 0x333333);
 		this.scene.add(ambientLight);
 
-		var light = new THREE.PointLight( 0xffffff, 1, 20 ); 
-		light.position.set(5, 5, 5); 
+		var light = new THREE.PointLight( 0xffffff, 1, 8 );
+		light.position.set(0, 0, 0);
 		this.scene.add(light);
+
+		this.camera.add(light); // light follows player
 
 		light = new THREE.PointLight( 0xffffff, 1, 10 ); 
 		light.position.set(this.chunkWidth - 5, 5, this.chunkWidth - 5); 
