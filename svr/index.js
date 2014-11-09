b8ad1a5161113fead076989b2d13a3112e62cb7c
Mr Speaker <mrspeaker@gmail.com>
2014-11-09 10:23:21 -0500

    nicer chunk gen.

--- svr/index.js
+++ svr/index.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var players = [];
+
 app.get('/', function(req, res){
 	res.sendFile('index.html', {'root': '../'});
 });
@@ -11,17 +13,28 @@ app.use("/src", express.static(__dirname + '/../src/'));
 app.use("/res", express.static(__dirname + '/../res/'));
 app.use("/lib", express.static(__dirname + '/../lib/'));
 
-io.on('connection', function(socket){
-	console.log('User connected');
-	socket.on('disconnect', function(){
-		console.log('User disconnected');
+io.on('connection', function(client){
+
+	client.userid = Math.random() * 99999999 | 0;
+	console.log('network:: player ' + client.userid + ' connected');
+
+	client.on('disconnect', function () {
+		console.log('\t network:: client disconnected ' + client.userid );
 	});
 
-	socket.on('hit', function(pos) {
+	client.emit('onconnected', { id: client.userid } );
+
+	client.on('hit', function(pos) {
 		console.log('hit at:', pos.x, pos.y, pos.z);
+	});
+
+	client.on("join", function (id) {
+		players.push([{id: id, client: client}]);
 	});
 });
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
+
+
